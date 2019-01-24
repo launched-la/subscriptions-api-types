458c10ecd12a2d10a9c6e9cc03e7eff4ba8e5daf
Ethan Jon <e10jc@users.noreply.github.com>
2019-01-24 13:50:07 -0800

    no ChildInput

--- @types/launched.d.ts
+++ @types/launched.d.ts
@@ -1,13 +1,5 @@
 declare namespace Launched {
   interface Child {
-    id: number,
-    name: string,
-    gender: string,
-    birthdate: string,
-    size: string,
-  }
-
-  interface ChildInput {
     name: string,
     gender: string,
     birthdate: string,
