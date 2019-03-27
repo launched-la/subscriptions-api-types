e26483c3ed92195ac8b7416b22b5963ae9da8bc0
e10jc <ethanczahor@gmail.com>
2019-03-27 11:53:04 -0700

    shipping_interval_frequency mistake

--- @types/recharge.d.ts
+++ @types/recharge.d.ts
@@ -205,7 +205,6 @@ declare namespace Recharge {
     product_title: string,
     properties: {name: string, value: string}[],
     quantity: number,
-    shipping_interval_frequency: string,
     shopify_product_id: number,
     shopify_variant_id: number,
     sku: string,
