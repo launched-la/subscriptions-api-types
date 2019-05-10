65be135c76fefb18d243fa42eced74b134c23631
e10jc <ethanczahor@gmail.com>
2019-05-10 09:49:50 -0700

    added returnly

---
+++ @types/returnly.d.ts
@@ -0,0 +1,28 @@
+declare namespace Returnly {
+  interface Return {
+    rma: string,
+    ext_store_id: string,
+    shopper_email: string,
+    currency: string,
+    status: 'authorized' | 'in_transit' | 'delivered' | 'pending_refund' | 'refunded',
+    order_shipping_refund: string,
+    return_shipping_quote_amount: string,
+    return_shipping_refund_amount: string,
+    return_shipping_paid_amount: string,
+    subtotal_amount: string,
+    estimated_refund_amount: string,
+    refund_amount: string,
+    restocking_fee_amount: string,
+    tax_amount: string,
+    discount_amount: string,
+    shopper_message: string,
+    merchant_notes: string,
+    is_exchange: boolean,
+    is_gift: boolean,
+    is_exempt_from_shipping: boolean,
+    return_label_amount: string,
+    created_at: string,
+    updated_at: string,
+    refunded_at: string,
+  }
+}
--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,5 @@
 /// <reference path="@types/launched.d.ts" />
 /// <reference path="@types/recharge.d.ts" />
+/// <reference path="@types/returnly.d.ts" />
 /// <reference path="@types/shopify.d.ts" />
 /// <reference path="@types/stripe.d.ts" />
