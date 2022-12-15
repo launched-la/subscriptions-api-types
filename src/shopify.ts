28fadb18d6dbc9d3a6a24d99aa2ac96b430ea6e2
Matt Dean <mdean400@gmail.com>
2022-12-15 14:43:30 -0700

    Replace Metafield value_type property with type

--- src/shopify.ts
+++ src/shopify.ts
@@ -197,7 +197,27 @@ export interface Metafield {
   key: string
   namespace: string
   value: string | number
-  value_type: 'integer' | 'string' | 'json_string'
+  type:
+    | 'boolean'
+    | 'collection_reference'
+    | 'color'
+    | 'date'
+    | 'date_time'
+    | 'dimension'
+    | 'file_reference'
+    | 'json'
+    | 'money'
+    | 'multi_line_text_field'
+    | 'number_decimal'
+    | 'number_integer'
+    | 'page_reference'
+    | 'product_reference'
+    | 'rating'
+    | 'single_line_text_field'
+    | 'url'
+    | 'variant_reference'
+    | 'volume'
+    | 'weight'
 }
 
 export interface Product {
